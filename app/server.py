42cfc8bb87732e31f47ff656c35b09b200cabe41
yusufbrima <yusufbrima@gmail.com>
2020-03-23 12:08:34 +0600

    Chest X-Ray COVID-19 Classifier

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/open?id=1-5d_qe7nMH3v_FyBjd1vwflo8PPr_1o0'
+export_file_name = 'covid_19_trained_model50.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['covid', 'normal']
 path = Path(__file__).parent
 
 app = Starlette()
